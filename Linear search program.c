eba4312f57c5c783fa83e8d51e3b07086546f2e5
Shampy <157746114+codershampy@users.noreply.github.com>
2024-04-11 00:16:50 +0530

    Add files via upload

---
+++ Linear search program.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int linear_search(int arr[],int n,int target){
+for(int i=0;i<n;i++)
+{
+    if(arr[i]==target){
+        return i;
+    }
+    else{
+        return -1;
+    }
+}
+}
+int main()
+{
+    int arr[]={10,20,30,40,50};
+    int n= sizeof(arr)/sizeof(arr[0]);
+    int target = 30;
+    int result= linear_search( arr , n,target);
+    if( result!=-1)
+    {
+        printf("Element found at index:%d\n",result);
+    }
+    else{
+        printf("Element not found\n");
+    }
+   return 0;
+}
+
